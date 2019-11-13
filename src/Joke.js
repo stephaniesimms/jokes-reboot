8da8ea9d900840b7098fff6d83e93dc4d2ae459e
Stephanie Simms <stephanie.simms@gmail.com>
2019-11-13 09:38:49 -0800

    retrieving jokes from API at mount

--- src/Joke.js
+++ src/Joke.js
@@ -8,7 +8,7 @@ class Joke extends Component {
   }
 
   handleUpVote() {
-
+    
   }
 
   handleDownVote() {
--- src/JokeList.js
+++ src/JokeList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import uuid from 'uuid/v4';
 import Joke from './Joke';
 
 class JokeList extends Component {
@@ -8,9 +10,46 @@ class JokeList extends Component {
       jokes: [],
       isLoading: false
     };
+    this.generateNewJokes = this.generateNewJokes.bind(this);
   }
-  
+
+  /* at mount, get jokes */
+  componentDidMount() {
+    if (this.state.jokes.length === 0) this.generateNewJokes();
+  }
+
+  // fetch jokes from API
+  async fetchJokes() {
+    try {
+      let jokes = this.state.jokes;
+
+      while (jokes.length < 10) {
+        let response = await axios.get('https://icanhazdadjoke.com', {
+          headers: { Accept: 'application/json' }
+        });
+        let joke = response.data.joke;
+
+        jokes.push({ joke: joke, id: uuid(), votes: 0 })
+      }
+      this.setState({ jokes: jokes, isLoading: false })
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // empty joke list, set isLoading to true, and then fetch jokes from API
+  generateNewJokes() {
+    this.setState(
+      { isLoading: true, jokes: [] }, 
+      // second argument to .setState is a fn to call when state is set
+      // this ensures user will see loading spinner while the request
+      // is running
+      this.fetchJokes
+    );
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div>Hi </div>
     )
